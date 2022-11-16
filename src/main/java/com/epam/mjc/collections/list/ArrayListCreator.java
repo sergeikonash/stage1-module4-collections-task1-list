3b628b358f8c1f199fc39d7ef4c0e9f43c75de1a
sergeikonash <38048905+sergeikonash@users.noreply.github.com>
2022-11-16 22:28:44 +0100

    Update ArrayListCreator.java

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -1,5 +1,8 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
         ArrayList<String> list = new ArrayList<>();
