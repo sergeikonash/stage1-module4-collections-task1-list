314871026d34755eaafed1337b50a7eb6048814d
sergeikonash <38048905+sergeikonash@users.noreply.github.com>
2022-11-16 17:25:55 +0100

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (Integer integer : sourceList) {
+            if (integer % 2 != 0) {
+                list.addFirst(integer);
+            } else {
+                list.addLast(integer);
+            }
+        }
+        return list;
     }
 }
